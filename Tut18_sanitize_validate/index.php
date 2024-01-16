1e897228778a3297bf27472107a4732ed49d0cf7
Joy Brar <joybrar2001@gmail.com>
2024-01-16 21:12:14 +0530

    18 tuts done

---
+++ Tut18_sanitize_validate/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        username: <br/>
+        <input type="text" name="username"><br/>
+        age: <br/>
+        <input type="text" name="age"><br/>
+        email: <br/>
+        <input type="text" name="email"><br/>
+        <input type="submit" name="login" value="login"><br/>
+    </form>
+</body>
+</html>
+
+<?php
+    if(isset($_POST["login"])){
+        // $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        // $age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
+        // $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+        // echo "You email is {$email}";
+
+        $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
+        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+        
+        if(empty($age)){
+            echo "That number wasn't valid<br/>";
+        }
+        else{
+            echo "You are {$age} years old<br/>";
+        }
+
+        if(empty($email)){
+            echo "That email isn't valid<br/>";
+        }
+        else{
+            echo "Your email is : {$email}<br/>";
+        }
+    }
+?>
